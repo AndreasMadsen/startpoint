fd1c899459ce4a487776262a21069ee4f229c91b
Andreas Madsen <amwebdk@gmail.com>
2012-12-25 23:45:56 +0100

    [init] commit

---
+++ startpoint.js
@@ -0,0 +1,53 @@
+
+var stream = require('stream');
+var util = require('util');
+
+function Startpoint(input) {
+  if (!(this instanceof Startpoint)) return new Startpoint(input);
+
+  stream.Readable.call(this);
+
+  this.position = 0;
+
+  // will keep a long list of buffers
+  if (input instanceof Error) {
+    this.error = input;
+    this.buffer = null;
+  } else if (Buffer.isBuffer(input)) {
+    this.error = null;
+    this.buffer = input;
+  } else {
+    this.error = null;
+    this.buffer = new Buffer(input.toString());
+  }
+}
+module.exports = Startpoint;
+util.inherits(Startpoint, stream.Readable);
+
+Startpoint.prototype._read = function (size, callback) {
+  if (this.error) {
+    callback(this.error, null);
+    this.emit('close');
+    return;
+  }
+
+  // At this point we know that buffer is set
+  if (this.position + size > this.buffer.length) {
+    size = this.buffer.length - this.position;
+  }
+
+  // if there are no more data to read, send nul
+  if (this.position === this.buffer.length) {
+    callback(null, null);
+    this.emit('close');
+    return;
+  }
+
+  // slice out the buffer and update the the current position
+  var buffer = this.buffer.slice(this.position, this.position + size);
+  this.position += size;
+
+  // no there is no delay here, if that is needed
+  // use another stream module to create that delay.
+  callback(null, buffer);
+};
---
+++ test.js
@@ -0,0 +1,30 @@
+
+var test = require('tap').test;
+var startpoint = require('./startpoint.js');
+var crypto = require('crypto');
+
+var buffer = crypto.randomBytes(16384 * 3.5);
+
+test('simple read and end', function (t) {
+  var point = startpoint(buffer);
+
+  t.equal(point.read(100).toString('hex'), buffer.slice(0, 100).toString('hex'));
+  t.equal(point.read(16384).toString('hex'), buffer.slice(100, 100 + 16384).toString('hex'));
+  t.equal(point.read(16384).toString('hex'), buffer.slice(100 + 16384, 100 + 2 * 16384).toString('hex'));
+  t.equal(point.read(16384).toString('hex'), buffer.slice(100 + 2 * 16384, 100 + 3 * 16384).toString('hex'));
+  t.equal(point.read(16384).toString('hex'), buffer.slice(100 + 3 * 16384, buffer.length).toString('hex'));
+  t.equal(point.read(16384), null);
+
+  t.end();
+});
+
+test('simple error sending', function (t) {
+  var fakeError = new Error('error');
+
+  var point = startpoint(fakeError);
+  point.once('error', function (err) {
+    t.equal(err, fakeError);
+    t.end();
+  });
+  point.read();
+});
